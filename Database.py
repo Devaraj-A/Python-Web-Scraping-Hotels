601cc73ea1c699968d9e79f93b34c2c955998a98
Devaraj A <63142998+Devaraj-A@users.noreply.github.com>
2022-04-24 18:54:04 +0530

    Update and rename database.py to Database.py

--- Database.py
+++ Database.py
@@ -1,10 +1,9 @@
 import pyodbc
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER=DESKTOP-OO651DA\SQLEXPRESS;DATABASE=Airbnb;Trusted_connection=yes')
+conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='';DATABASE=Airbnb;Trusted_connection=yes')
 conn.autocommit=True
 cursor=conn.cursor()
 #cursor.execute('CREATE DATABASE Airbnb')
-cursor.execute('Use Airbnb')
-#cursor.execute('select table hotels')
+#cursor.execute('Use Airbnb')
 #cursor.execute('Create table hotels(ID INT,Name VARCHAR,Description VARCHAR,Rating VARCHAR,Price VARCHAR,Ref_url VARCHAR)')
 #conn.commit()
